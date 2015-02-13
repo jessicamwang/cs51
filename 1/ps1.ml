590b74a8dc1cdcc44d97849a82e6801b14376818
Charles Liu <charlesliu@college.harvard.edu>
2015-02-13 17:10:47 -0500

    PS1 submission

--- 1/ps1.ml
+++ 1/ps1.ml
@@ -1,9 +1,9 @@
 (*** CS 51 Problem Set 1 ***)
 (*** February 6, 2015 ***)
-(*** YOUR NAME HERE ***)
+(*** Charles Liu ***)
 
-(* Open up the library we'll be using in this course *)
 open Core.Std
+
 
 (* Problem 1 - Fill in types:
  * Replace each ??? with the appropriate type of the corresponding expression.
@@ -11,19 +11,13 @@ open Core.Std
  * before submission. *)
 
 (*>* Problem 1a *>*)
-(*
-let prob1a : ??? = let greet y = "Hello " ^ y in greet "World!";;
-*)
+let prob1a : bytes = let greet y = "Hello " ^ y in greet "World!";;
 
 (*>* Problem 1b *>*)
-(*
-let prob1b : ??? = [Some 7; Some 1; None; Some 1];;
-*)
+let prob1b : int option list = [Some 7; Some 1; None; Some 1];;
 
 (*>* Problem 1c *>*)
-(*
-let prob1c : ??? = ((None, Some 42.0), true);;
-*)
+let prob1c : ('a option * float option) * bool = ((None, Some 42.0), true);;
 
 
 (* Explain in a comment why the following will not type check,
@@ -33,6 +27,11 @@ let prob1c : ??? = ((None, Some 42.0), true);;
 (*
 let prob1d : string * int list = [("CS", 51); ("CS", 50)];;
 *)
+(* This will not type check because the type is read as a pair with a string
+ * and a list of ints, when it really should be a list of pairs, each with a
+ * string and int. *)
+(* Problem 1d fix *)
+let prob1d : (string * int) list = [("CS", 51); ("CS", 50)];;
 
 (*>* Problem 1e *>*)
 (*
@@ -40,6 +39,13 @@ let prob1e : int =
   let compare (x,y) = x < y in
   if compare (5, 4.9) then 5 else 2;;
 *)
+(* This will not type check because compare expects x and y to be of the same
+ * type, and when compare (5, 4.9) is called it is given an int and a float,
+ * which are different types. *)
+(* Problem 1e fix *)
+let prob1e : int =
+  let compare (x,y) = x < y in
+  if compare (5., 4.9) then 5 else 2;;
 
 (*>* Problem 1f *>*)
 (*
@@ -48,158 +54,254 @@ let prob1f : (string * string) list =
    ("May", None); ("June", 1); ("July", 4); ("August", None);
    ("September", 3); ("October", 1); ("November", 2); ("December", 11)] ;;
 *)
+(* This will not type check because the second argument of each pair is 
+ * an int option, not a string. Also, the integers by themselves are a
+ * different type from None, so they must be changed to int options by
+ * using the Some constructor. *)
+(* Problem 1f fix *)
+let prob1f : (string * int option) list =
+  [("January", None); ("February", None); ("March", Some 15); ("April", None);
+   ("May", None); ("June", Some 1); ("July", Some 4); ("August", None);
+   ("September", Some 3); ("October", Some 1); ("November", Some 2);
+   ("December", Some 11)] ;;
 
 
 
 (* Problem 2 - Write the following functions *)
-(* For each subproblem, you must implement a given function and corresponding
- * unit tests (i.e. assert expressions). You are provided a high level
- * description as well as a prototype of the function you must implement. *)
 
 (*>* Problem 2a *>*)
 
-(* `reversed lst` should return true if the integers in lst are in
- * decreasing order. The empty list is considered to be reversed. Consecutive
- * elements can be equal in a reversed list. *)
+(* reversed: takes a list of integers and returns true if the integers in the
+ * list are in decreasing order. The empty list is considered to be reversed.
+ * Consecutive elements can be equal in a reversed list. *)
+let rec reversed (lst : int list) : bool =
+  match lst with
+  | [] -> true
+  | hd :: tl -> 
+    (match tl with
+     | [] -> true
+     | tl_value :: _ -> hd >= tl_value && reversed tl)
+;;
 
-(* Here is its prototype/signature: *)
-(* reversed : int list -> bool *)
+(* Problem 2a unit tests *)
 
-(* Implement reversed below, and be sure to write tests for it (see 2b for
- * examples of tests). *)
+let () = assert ((reversed []) = true);;
+let () = assert ((reversed [10]) = true);;
+let () = assert ((reversed [1;5;3]) = false);;
+let () = assert ((reversed [7;6;5;4;3;2;1]) = true);;
+let () = assert ((reversed [7;7;5;5;5;2;1]) = true);;
+let () = assert ((reversed [5;5;7;5;5;2;1]) = false);;
+let () = assert ((reversed [7;6;5;4;3;1;2]) = false);;
+let () = assert ((reversed [5;-1;-9;-100]) = true);;
+let () = assert ((reversed [10;-5;0;-100]) = false);;
 
 
 (*>* Problem 2b *>*)
 
-(* merge takes two integer lists, each sorted in increasing order,
- and returns a single merged list in sorted order. For example:
+(* merge: takes two integer lists, each sorted in increasing order,
+ and returns a single merged list in sorted order *)
+let rec merge (lst1 : int list) (lst2 : int list) : int list =
+  match (lst1,lst2) with
+  | ([],_) -> lst2
+  | (_,[]) -> lst1
+  | (hd1 :: tl1, hd2 :: tl2) ->
+    if hd1 <= hd2 then hd1 :: merge tl1 lst2
+    else hd2 :: merge lst1 tl2
+;;
 
-merge [1;3;5] [2;4;6];;
-- : int list = [1; 2; 3; 4; 5; 6]
-merge [1;3;5] [2;4;6;12];;
-- : int list = [1; 2; 3; 4; 5; 6; 12]
-merge [0;3;5;711;747] [2;4;6;12];;
-- : int list = [0; 2; 3; 4; 5; 6; 12; 711; 747]
-
-*)
-
-(* The type signature for merge is as follows: *)
-(* merge : int list -> int list -> int list *)
-
-(*
-(* sample tests *)
+(* Problem 2b unit tests *)
 let () = assert ((merge [1;2;3] [4;5;6;7]) = [1;2;3;4;5;6;7]);;
-let () = assert ((merge [4;5;6;7] [1;2;3]) = [1;2;3;4;5;6;7]);;
 let () = assert ((merge [4;5;6;7] [1;2;3]) = [1;2;3;4;5;6;7]);;
 let () = assert ((merge [2;2;4;4] [1;2;3]) = [1;2;2;2;3;4;4]);;
 let () = assert ((merge [1;3] [1;3]) = [1;1;3;3]);;
 let () = assert ((merge [-1;2;3;42] [-1;6;1001]) = [-1;-1;2;3;6;42;1001]);;
+let () = assert ((merge [1] [1]) = [1;1]);;
+let () = assert ((merge [1;4;5;7] [2;3;6]) = [1;2;3;4;5;6;7]);;
 let () = assert ((merge [] []) = []);;
 let () = assert ((merge [1] []) = [1]);;
 let () = assert ((merge [] [-7]) = [-7]);;
 let () = assert ((merge [1] [-1]) = [-1;1]);;
-*)
 
 
 (*>* Problem 2c *>*)
-(* unzip should be a function which, given a list of pairs, returns a
- * pair of lists, the first of which contains each first element of
- * each pair, and the second of which contains each second element.
- * The returned lists should have the elements in the order in which
- * they appeared in the input. So, for instance:
 
-unzip [(1,2);(3,4);(5,6)];;
-- : int list * int list = ([1;3;5],[2;4;6])
+(* unzip: takes a list of pairs and returns a pair of lists, the
+ * first of which contains each first element of each pair, and
+ * the second of which contains each second element (all elements
+ * in the same order as in the input). *)
+let rec unzip (pair_lst : (int * int) list) : int list * int list =
+  match pair_lst with
+  | [] -> ([],[])
+  | (fst,snd) :: tl -> 
+    let (lst1, lst2) = unzip tl in
+    (fst :: lst1, snd :: lst2)
+;;
 
-*)
-
-
-(* The type signature for unzip is as follows: *)
-(* unzip : (int * int) list -> int list * int list) *)
+(* Problem 2c unit tests *)
+let () = assert ((unzip []) = ([],[]));;
+let () = assert ((unzip [(1,5)]) = ([1],[5]));;
+let () = assert ((unzip [(1,2);(3,4);(5,6)]) = ([1;3;5],[2;4;6]));;
+let () = assert ((unzip [(7,1);(-1,4);(3,2)]) = ([7;-1;3],[1;4;2]));;
+let () = assert ((unzip [(3,1);(-1,4);(3,1)]) = ([3;-1;3],[1;4;1]));;
+let () = assert ((unzip [(7,1);(3,1);(-1,4);(3,1)]) 
+                 = ([7;3;-1;3],[1;1;4;1]));;
+let () = assert ((unzip [(1,1);(-1,4);(3,2);(7,0);(8,0);(10,10)]) 
+                 = ([1;-1;3;7;8;10],[1;4;2;0;0;10]));;
 
 
 (*>* Problem 2d *>*)
 
-(* `variance lst` returns None if lst has fewer than 2 floats, and
- * Some of the variance of the floats in lst otherwise.  Recall that
- * the variance of a sequence of numbers is 1/(n-1) * sum (x_i-m)^2,
- * where a^2 means a squared, and m is the arithmetic mean of the list
- * (sum of list / length of list). For example:
+(* len: returns the length of a list of generics *)
+let rec len (lst : 'a list) : int =
+  match lst with
+  | [] -> 0
+  | _ :: tl -> 1 + len tl
+;;
 
-variance [1.0; 2.0; 3.0; 4.0; 5.0];;
-- : int option = Some 2.5
-variance [1.0];;
-- : int option = None
+(* sum: returns the sum of a list of floats *)
+let rec sum (lst : float list) : float =
+  match lst with
+  | [] -> 0.
+  | hd :: tl -> hd +. sum tl
+;;
 
- * Remember to use the floating point version of the arithmetic
- * operators when operating on floats (+. *., etc). The "float"
- * function can cast an int to a float. *)
+(* arith_mean: returns the arithmetic mean of a list of floats. *)
+let arith_mean (lst : float list) : float =
+  sum lst /. float (len lst)
+;;
 
-(* variance : float list -> float option *)
+(* square: returns the square of a float. *)
+let square (x : float) : float = x *. x;;
+
+(* variance: returns the variance of a list of floats (returns None if 
+ * fewer than 2 floats) as a float option. The variance is given by
+ * 1/(n-1) * sum (x_i-m)^2, where m is the arithmetic mean of the list. *)
+let variance (lst : float list) : float option =
+  match lst with
+  | [] -> None
+  | [_] -> None
+  | lst -> 
+    let mean = arith_mean lst in
+    let rec sum_squared_error (lst' :float list) : float = 
+      match lst' with
+      | [] -> 0.
+      | hd :: tl -> square (hd -. mean) +. sum_squared_error tl
+    in
+    Some ((sum_squared_error lst) /. (float (len lst - 1)))
+;;
+
+
+(* float_opt_equal: checks if the values of float options are equal 
+ * or if both None. *)
+let float_opt_equal (x : float option) (y : float option) : bool =
+  match (x,y) with
+  | (None,None) -> true
+  | (None,_) -> false
+  | (_,None) -> false
+  | (Some a,Some b) -> (a -. b < 0.00001) && (b -. a < 0.00001)
+;;
+
+(* Problem 2d unit tests *)
+let () = assert (float_opt_equal (variance []) None);;
+let () = assert (float_opt_equal (variance [1.0]) None);;
+let () = assert (float_opt_equal (variance [1.0; 2.0]) (Some 0.5));;
+let () = assert (float_opt_equal (variance [1.0; 2.0; 3.0; 4.0; 5.0]) 
+                                 (Some 2.5));;
+let () = assert (float_opt_equal (variance [1.0; 1.0; 1.0]) (Some 0.));;
+let () = assert (float_opt_equal (variance [-3.0; -2.0; -4.0; -1.0; -5.0]) 
+                                 (Some 2.5));;
+let () = assert (float_opt_equal (variance [1.0; 7.0; 3.0; 3.5; 6.8]) 
+                                 (Some 6.688));;
 
 
 (*>* Problem 2e *>*)
 
-(* few_divisors n m should return true if n has fewer than m divisors,
- * (including 1 and n) and false otherwise. Note that this is *not* the
- * same as n having fewer divisors than m:
+(* few_divisors n m: returns true if n has fewer than m divisors,
+ * (including 1 and n) and false otherwise. *)
+let few_divisors (n : int) (m : int) : bool =
+  let rec divisor_count divisor count =
+    match divisor with
+    | 0 -> count
+    | _ -> 
+      if n % divisor = 0 then divisor_count (divisor - 1) (count + 1)
+      else divisor_count (divisor - 1) count
+  in
+  divisor_count n 0 < m
+;;
 
-few_divisors 23 3;;
-- : bool = true
-few_divisors 12 6;;
-- : bool = false
-few_divisors 12 7;;
-- : bool = true
-
- * Do not worry about negative integers at all. We will not test
- * your code using negative values for n and m, and do not
- * consider negative integers for divisors (e.g. don't worry about
- * -2 being a divisor of 4) *)
-
-(* The type signature for few_divisors is: *)
-(* few_divisors : int -> int -> bool *)
-
+(* Problem 2e unit tests *)
+let () = assert ((few_divisors 1 1) = false);;
+let () = assert ((few_divisors 1 2) = true);;
+let () = assert ((few_divisors 2 2) = false);;
+let () = assert ((few_divisors 2 3) = true);;
+let () = assert ((few_divisors 6 4) = false);;
+let () = assert ((few_divisors 6 5) = true);;
+let () = assert ((few_divisors 10 3) = false);;
+let () = assert ((few_divisors 10 10) = true);;
 
 (*>* Problem 2f *>*)
 
-(* `concat_list sep lst` returns one big string with all the string
- * elements of lst concatenated together, but separated by the string
- * sep. Here are some example tests:
+(* concat_list: returns one string with all the string elements of
+ * lst concatenated together separated by the string sep. *)
 
-concat_list ", " ["George"; "Beth"; "Ned"];;
-- : string = "George, Beth, Ned"
-concat_list "..." ["Moo"; "Baaa"; "Quack"];;
-- : string = "Moo...Baaa...Quack"
-concat_list ", " [];;
-- : string = ""
-concat_list ", " ["Moo"];;
-- : string = "Moo"
+let rec concat_list (sep : string) (lst : string list) : string = 
+  match lst with
+  | [] -> ""
+  | [str] -> str
+  | hd :: tl -> hd ^ sep ^ concat_list sep tl
+;;
 
-*)
-
-(* The type signature for concat_list is: *)
-(* concat_list : string -> string list -> string *)
+(* Problem 2f unit tests *)
+let () = assert ((concat_list "..." []) = "");;
+let () = assert ((concat_list "..." ["Hi"]) = "Hi");;
+let () = assert ((concat_list ", " ["Hello";"World!"]) = "Hello, World!");;
+let () = assert ((concat_list "*" ["1";"2";"3"]) = "1*2*3");;
+let () = assert ((concat_list "." ["^";"^";"O";"o"]) = "^.^.O.o");;
+let () = assert ((concat_list "..." ["Old";"Man";"Sea"]) = "Old...Man...Sea");;
 
 (*>* Problem 2g *>*)
 
-(* One way to compress a list of characters is to use run-length encoding.
- * The basic idea is that whenever we have repeated characters in a list
- * such as ['a';'a';'a';'a';'b';'b';'b';'b';'c';'d';'d';'d';'d'] we can
- * (sometimes) represent the same information more compactly as a list
- * of pairs like [(4,'a');(4,'b');(1,'c');(4,'d')].  Here, the numbers
- * represent how many times the character is repeated.  For example,
- * the first character in the string is 'a' and it is repeated 4 times,
- * followed by 4 occurrences of the character 'b', followed by one 'c',
- * and finally 4 copies of 'd'.
- *
- * Write a function to_run_length that converts a list of characters into
- * the run-length encoding, and then write a function from_run_length
- * that converts back. Writing both functions will make it easier to
- * test that you've gotten them right. *)
+(* to_run_length: generates the run-length encoding of a string *)
+let rec to_run_length (lst : char list) : (int * char) list = 
+  match lst with
+  | [] -> []
+  | hd :: tl -> 
+    (match tl with
+     | [] -> [(1,hd)]
+     | next :: _ -> 
+       if hd = next then 
+         match to_run_length tl with
+         | [] -> []
+         | (count,ch) :: run_tl ->
+           (1+count,ch) :: run_tl         
+       else (1,hd) :: to_run_length tl)
+;;
 
-(* The type signatures for to_run_length and from_run_length are: *)
-(* to_run_length : char list -> (int * char) list *)
-(* from_run_length : (int * char) list -> char list *)
+(* from_run_length: generates a string from a run-length encoding *)
+let rec from_run_length (runs : (int * char) list) : char list =
+  match runs with
+  | [] -> []
+  | (len,ch) :: tl -> 
+    if len = 0 then from_run_length tl
+    else ch :: from_run_length ((len-1,ch) :: tl)
+;;
+
+(* Problem 2g unit tests *)
+let () = assert ((to_run_length []) = []);;
+let () = assert ((to_run_length ['a']) = [(1,'a')]);;
+let () = assert ((to_run_length ['a';'a']) = [(2,'a')]);;
+let () = assert ((to_run_length ['a';'b';'c']) = [(1,'a');(1,'b');(1,'c')]);;
+let () = assert ((to_run_length ['a';'a';'b';'c';'c';'c'])
+                 = [(2,'a');(1,'b');(3,'c')]);;
+let () = assert ((to_run_length ['a';'b';'a']) = [(1,'a');(1,'b');(1,'a')]);;
+let () = assert ((from_run_length []) = []);;
+let () = assert ((from_run_length [(1,'a')]) = ['a']);;
+let () = assert ((from_run_length [(2,'a')]) = ['a';'a']);;
+let () = assert ((from_run_length [(1,'a');(1,'b');(1,'c')]) = ['a';'b';'c']);;
+let () = assert ((from_run_length[(2,'a');(1,'b');(3,'c')] )
+                 = ['a';'a';'b';'c';'c';'c']);;
+let () = assert ((from_run_length [(1,'a');(1,'b');(1,'a')]) = ['a';'b';'a']);;
+
 
 (*>* Problem 3 *>*)
 
@@ -224,3 +326,39 @@ concat_list ", " ["Moo"];;
 
 (* The type signature for permuations is: *)
 (* permutations : int list -> int list list *)
+
+(* interleave: interleaves x to all possible positions in lst *)
+let interleave (x : int) (lst : int list) : int list list =
+  let rec inter (bef : int list) (aft : int list) : int list list =
+    (bef @ (x :: aft)) :: 
+    match aft with
+    | [] -> []
+    | aft_hd :: aft_tl -> inter (bef @ [aft_hd]) aft_tl
+  in
+  inter [] lst
+;;
+
+(* permutations: generates all permutations of a list of ints *)
+let rec permutations (lst : int list) : int list list =
+  match lst with
+  | [] -> [[]]
+  | hd :: tl -> 
+    List.concat (List.map (permutations tl) ~f:(fun lst' -> interleave hd lst'))
+;;
+
+(* Problem 3 unit tests *)
+let () = assert ((permutations []) = [[]]);;
+let () = assert ((permutations [1]) = [[1]]);;
+let () = assert ((permutations [1;2]) = [[1;2];[2;1]]);;
+let () = assert ((permutations [1;2;3]) = [[1;2;3];[2;1;3];[2;3;1];
+                                           [1;3;2];[3;1;2];[3;2;1]]);;
+let () = assert ((permutations [1;5;7]) = [[1;5;7];[5;1;7];[5;7;1];
+                                           [1;7;5];[7;1;5];[7;5;1]]);;
+let () = assert ((permutations [1;2;3;4]) = [[1;2;3;4];[2;1;3;4];[2;3;1;4];
+                                             [2;3;4;1];[1;3;2;4];[3;1;2;4];
+                                             [3;2;1;4];[3;2;4;1];[1;3;4;2];
+                                             [3;1;4;2];[3;4;1;2];[3;4;2;1];
+                                             [1;2;4;3];[2;1;4;3];[2;4;1;3];
+                                             [2;4;3;1];[1;4;2;3];[4;1;2;3];
+                                             [4;2;1;3];[4;2;3;1];[1;4;3;2];
+                                             [4;1;3;2];[4;3;1;2];[4;3;2;1]]);;
