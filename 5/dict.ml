c6981de791ee9886c4548e7dd1542fad37efa41b
Allison Buchholtz-Au <abuchholtzau@college.harvard.edu>
2015-03-26 23:52:46 -0400

    another try at remove

--- 5/dict.ml
+++ 5/dict.ml
@@ -180,7 +180,7 @@ struct
     match d with
       | [] -> []
       | (k1,v1)::d1 ->
-  (match D.compare k k1 with
+	(match D.compare k k1 with
           | Equal -> d1
           | Greater -> (k1,v1)::(remove d1 k)
           | _ -> d)
@@ -475,11 +475,7 @@ struct
       | Up(l,(k1,v1),r) -> Two(l,(k1,v1),r)
       | Done x -> x
 
-  (* Upward phase for removal where the parent of the hole is a Two node.
-   * See cases (1-2) on the handout. n is the (key,value) pair contained in
-   * the parent node; left and right are the subtrees of the parent node (our
-   * hole is one of these subtrees); and dir indicates which subtree was
-   * contained by the hole. *)
+  (* DO NOT EDIT THIS *)
   let remove_upward_two (n: pair) (rem: pair option)
       (left: dict) (right: dict) (dir: direction2) : hole =
     match dir,n,left,right with
@@ -491,14 +487,10 @@ struct
    Absorbed(rem,Two(Two(a,x,b),y,Two(c,z,d)))
       | Left2,_,_,_ | Right2,_,_,_ -> Absorbed(rem,Two(Leaf,n,Leaf))
 
-  (* Upward phase for removal where the parent of the hole is a Three node.
-   * See cases (3-4) on the handout. n1 and n2 are the (key,value) pairs
-   * contained in the parent node; left, middle, and right are the subtrees
-   * of the parent node (our hole is one of these subtrees); and dir indicates
-   * which subtree was the tree contained by the hole. *)
+  (* DO NOT EDIT THIS *)
   let remove_upward_three (n1: pair) (n2: pair) (rem: pair option)
       (left: dict) (middle: dict) (right: dict) (dir: direction3) : hole =
-    match dir,n1,n2,left,middle,right with
+      match dir,n1,n2,left,middle,right with
       | Left3,x,z,a,Two(b,y,c),d -> Absorbed(rem,Two(Three(a,x,b,y,c),z,d))
       | Mid3,y,z,Two(a,x,b),c,d -> Absorbed(rem,Two(Three(a,x,b,y,c),z,d))
       | Mid3,x,y,a,b,Two(c,z,d) -> Absorbed(rem,Two(a,x,Three(b,y,c,z,d)))
