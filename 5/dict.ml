cd503f2711ddcc996c1d2c5cb81fd74d63412bd6
Allison Buchholtz-Au <abuchholtzau@college.harvard.edu>
2015-03-26 23:42:54 -0400

    improved removed

--- 5/dict.ml
+++ 5/dict.ml
@@ -180,7 +180,7 @@ struct
     match d with
       | [] -> []
       | (k1,v1)::d1 ->
-	(match D.compare k k1 with
+  (match D.compare k k1 with
           | Equal -> d1
           | Greater -> (k1,v1)::(remove d1 k)
           | _ -> d)
@@ -475,17 +475,27 @@ struct
       | Up(l,(k1,v1),r) -> Two(l,(k1,v1),r)
       | Done x -> x
 
-  (* DO NOT EDIT THIS *)
+  (* Upward phase for removal where the parent of the hole is a Two node.
+   * See cases (1-2) on the handout. n is the (key,value) pair contained in
+   * the parent node; left and right are the subtrees of the parent node (our
+   * hole is one of these subtrees); and dir indicates which subtree was
+   * contained by the hole. *)
   let remove_upward_two (n: pair) (rem: pair option)
       (left: dict) (right: dict) (dir: direction2) : hole =
     match dir,n,left,right with
       | Left2,x,l,Two(m,y,r) -> Hole(rem,Three(l,x,m,y,r))
       | Right2,y,Two(l,x,m),r -> Hole(rem,Three(l,x,m,y,r))
-      | Left2,x,a,Three(b,y,c,z,d) -> Absorbed(rem,Two(Two(a,x,b),y,Two(c,z,d)))
-      | Right2,z,Three(a,x,b,y,c),d -> Absorbed(rem,Two(Two(a,x,b),y,Two(c,z,d)))
+      | Left2,x,a,Three(b,y,c,z,d) ->
+   Absorbed(rem,Two(Two(a,x,b),y,Two(c,z,d)))
+      | Right2,z,Three(a,x,b,y,c),d ->
+   Absorbed(rem,Two(Two(a,x,b),y,Two(c,z,d)))
       | Left2,_,_,_ | Right2,_,_,_ -> Absorbed(rem,Two(Leaf,n,Leaf))
 
-  (* DO NOT EDIT THIS *)
+  (* Upward phase for removal where the parent of the hole is a Three node.
+   * See cases (3-4) on the handout. n1 and n2 are the (key,value) pairs
+   * contained in the parent node; left, middle, and right are the subtrees
+   * of the parent node (our hole is one of these subtrees); and dir indicates
+   * which subtree was the tree contained by the hole. *)
   let remove_upward_three (n1: pair) (n2: pair) (rem: pair option)
       (left: dict) (middle: dict) (right: dict) (dir: direction3) : hole =
     match dir,n1,n2,left,middle,right with
@@ -494,13 +504,13 @@ struct
       | Mid3,x,y,a,b,Two(c,z,d) -> Absorbed(rem,Two(a,x,Three(b,y,c,z,d)))
       | Right3,x,z,a,Two(b,y,c),d -> Absorbed(rem,Two(a,x,Three(b,y,c,z,d)))
       | Left3,w,z,a,Three(b,x,c,y,d),e ->
-        Absorbed(rem,Three(Two(a,w,b),x,Two(c,y,d),z,e))
+   Absorbed(rem,Three(Two(a,w,b),x,Two(c,y,d),z,e))
       | Mid3,y,z,Three(a,w,b,x,c),d,e ->
-        Absorbed(rem,Three(Two(a,w,b),x,Two(c,y,d),z,e))
+   Absorbed(rem,Three(Two(a,w,b),x,Two(c,y,d),z,e))
       | Mid3,w,x,a,b,Three(c,y,d,z,e) ->
-        Absorbed(rem,Three(a,w,Two(b,x,c),y,Two(d,z,e)))
+   Absorbed(rem,Three(a,w,Two(b,x,c),y,Two(d,z,e)))
       | Right3,w,z,a,Three(b,x,c,y,d),e ->
-        Absorbed(rem,Three(a,w,Two(b,x,c),y,Two(d,z,e)))
+   Absorbed(rem,Three(a,w,Two(b,x,c),y,Two(d,z,e)))
       | Left3,_,_,_,_,_ | Mid3,_,_,_,_,_ | Right3,_,_,_,_,_ ->
         Absorbed(rem,Three(Leaf,n1,Leaf,n2,Leaf))
 
