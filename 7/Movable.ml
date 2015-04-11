afa5b6b9ab80308036f1ba77daad5d7bfd78a39e
Charles Liu <charlesliu@college.harvard.edu>
2015-04-11 13:16:57 -0400

    movable

--- 7/Movable.ml
+++ 7/Movable.ml
@@ -1,6 +1,7 @@
 open Helpers
 open WorldObject
 open WorldObjectI
+open Event
 
 (** Class type for objects which constantly try to move in a calculated next
     direction. *)
@@ -21,18 +22,25 @@ object (self)
   (***********************)
 
   (* ### TODO: Part 2 Movement ### *)
+  initializer
+    match inv_speed with
+    | None -> self#register_handler World.move_event (fun _ -> ())
+    | Some x -> self#register_handler (Event.buffer x World.move_event)
+                  (fun _ -> self#do_move);
 
   (**************************)
   (***** Event Handlers *****)
   (**************************)
 
   (* ### TODO: Part 2 Movement ### *)
+  method private do_move = 
+    super#move self#next_direction
 
   (***************************)
   (***** Movable Methods *****)
   (***************************)
 
   (* ### TODO: Part 2 Movement ### *)
-  method next_direction = raise TODO
+  method next_direction = None
 
 end
